4bb2e838d3f804e5c5dfff370810e8c07cb2a503
nathanso <nathan.so.96@gmail.com>
2014-11-22 13:26:30 +0000

    Added functions to the main file

---
+++ main.lua
@@ -0,0 +1,26 @@
+function love.load()
+end
+
+function love.draw()
+end
+
+function love.update()
+end
+
+function love.focus()
+end
+
+function love.keypressed()
+end
+
+function love.keyreleased()
+end
+
+function love.mousepressed()
+end
+
+function love.mousereleased()
+end
+
+function love.quit()
+end
